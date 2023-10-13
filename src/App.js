6d0268f2273c8fdf8d93b831e9f97cacbd279a3e
Josh Febbie <joshdfebbie@gmail.com>
2023-10-12 17:48:24 -0700

    first_sub

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import "./normal.css";
+
+function App() {
+  return (
+    <div className="App">
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          <span>+</span>
+          New Chat
+        </div>
+      </aside>
+      <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-message">
+          <div className="chat-message-center">
+            <div className="avatar">
+
+            </div>
+            <div className="message">
+                Hello World
+            </div>
+          </div>
+         </div>
+         <div className="chat-message chatgpt">
+          <div className="chat-message-center">
+            <div className="avatar">
+
+            </div>
+            <div className="message">
+                I am a chatbot
+            </div>
+          </div>
+         </div>
+        </div>
+        <div className="chat-input-holder">
+          <textarea rows="1" className="chat-input-textarea"></textarea>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
